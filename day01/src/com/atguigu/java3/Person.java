161fe4b16ddf1b6fc31735ba98535b0f3a46df4c
renyushu <renyushu2016@gmail.com>
2022-10-20 17:07:17 +0800

    java 监听器

--- day01/src/com/atguigu/java3/Person.java
+++ day01/src/com/atguigu/java3/Person.java
@@ -12,14 +12,17 @@ public class Person {
     //在成员变量定义一个监听器对象
     private PersonListener personListener;
 
+    //对象方法
     //在事件源中定义两个方法
     public void Eat() {
         // 当事件源调用了Eat方法时，应该触发监听器的方法，调用监听器的方法并把事件对象传递进去
+        System.out.println("吃饭....");
         personListener.doEat(new Event(this));
     }
 
     public void sleep() {
         //当事件源调用了Eat方法时，应该触发监听器的方法，调用监听器的方法并把事件对象传递进去
+        System.out.println("睡觉....");
         personListener.doSleep(new Event(this));
     }
 
--- day01/src/com/atguigu/java3/PersonTest.java
+++ day01/src/com/atguigu/java3/PersonTest.java
@@ -1,10 +1,16 @@
 package com.atguigu.java3;
 
+/**
+ * 监听器就是一个实现特定接口的普通java程序，这个程序专门用于监听另一个java对象的方法调用或属性改变，
+ * 当被监听对象发生上述事件后，监听器某个方法将立即被执行。。
+ */
+
 public class PersonTest {
     public static void main(String[] args) {
         Person person = new Person();
 
         person.registerLister(new PersonListener() {
+            // 监听器方法
             @Override
             public void doEat(Event event) {
                 Person person1 = event.getResource();
@@ -20,5 +26,6 @@ public class PersonTest {
 
         //当调用eat方法时，触发事件，将事件对象传递给监视器，最后监听器获取事件源，对事件源进行操作
         person.Eat();
+        person.sleep();
     }
 }
